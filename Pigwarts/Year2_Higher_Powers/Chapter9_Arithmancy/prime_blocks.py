41c4e2500950742abbf57b3a6b463c81e3f134fe
Edwin Evans <edwin@Edwins-MacBook-Pro-2.local>
2016-10-27 11:57:15 -0700

    Fix typo

--- Pigwarts/Year2_Higher_Powers/Chapter9_Arithmancy/prime_blocks.py
+++ Pigwarts/Year2_Higher_Powers/Chapter9_Arithmancy/prime_blocks.py
@@ -1,7 +1,7 @@
 print "\n" \
 	"Only one spell, executed with absolute precision, can conjure any number.\n" \
 	"Despite any rumors you may heard about ancient times, even the most\n" \
-	"powerful Arithmency practitioner can conjure any number any other way!" \
+	"powerful Arithmency practitioner cannot conjure any number any other way!" \
 	"\n"
 
 print 2
