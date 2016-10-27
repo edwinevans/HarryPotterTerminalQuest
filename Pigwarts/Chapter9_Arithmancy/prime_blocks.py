d074e8efcd6694654ac0a20e3e4a96f2ec37ba7f
Edwin Evans <edwin@Edwins-MacBook-Pro-2.local>
2016-10-27 11:24:16 -0700

    Started Arithmency

---
+++ Pigwarts/Chapter9_Arithmancy/prime_blocks.py
@@ -0,0 +1,17 @@
+print "\n" \
+	"Only one spell, executed with absolute precision, can conjure any number.\n" \
+	"Despite any rumors you may heard about ancient times, even the most\n" \
+	"powerful Arithmency practitioner can conjure any number any other way!" \
+	"\n"
+
+print 2
+print 3
+print 2 * 2
+print 5 * 1
+print 3 * 2
+print 7
+print 2 * 2 * 2
+print 3 * 3
+print 5 * 2
+
+print
