b82fae2f48865cc8597bfa8328d9304476f1b188
Mia Evans <miaevans@edwin-evanss-macbook-pro.local>
2016-11-13 16:54:08 -0800

    Add catastrophe

---
+++ Pigwarts/Year2_Catastrophe/robot_fire.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle.speed(1)
+
+turtle.penup()
+turtle.addshape("robot2.gif")
+turtle.shape("robot2.gif")
+
+turtle.backward(200)
+turtle.stamp()
+turtle.forward(400)
+
+turtle.done()
