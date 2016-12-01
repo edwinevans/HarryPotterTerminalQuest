4b50fefb854f7b28c90699400c508d0738f17a2c
Mia Evans <miaevans@edwin-evanss-macbook-pro.local>
2016-11-30 20:49:47 -0800

    misc

--- Pigwarts/Year3_Getting_Fancy/Chapter4_Lessons/loop7.py
+++ Pigwarts/Year3_Getting_Fancy/Chapter4_Lessons/loop7.py
@@ -4,12 +4,11 @@
 #
 # Hint 1: Do the first part and then the second
 # Hint 2: For powers of 2 a good approach is to use an extra variable that
-#	will keep getting bigger
+#	will keep getting bigger.
 
 '''
 Make the output look exactly like
 
-1
 4
 8
 12
@@ -26,3 +25,31 @@ Make the output look exactly like
 '''
 
 import random
+
+number = 0
+
+for i in range (5):
+	number = number + 4
+	print number
+
+print "---"
+
+number = 1
+for i in range(6):
+	print number
+	number = number * 2
+
+
+
+
+print ""
+print "Alternate way"
+print "---"
+
+for i in range (1, 6):
+	print i * 4
+
+print "---"
+
+for i in range (6):
+	print 2 ** i
