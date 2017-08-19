36905a9fb0fb9549f934447f0f070fde42189b6b
Edwin Evans <ed@edwinevans.me>
2017-08-19 13:29:41 -0700

    Add WaterPlants

---
+++ Pigwarts/Year4_Potions/WaterPlants/robot.py
@@ -0,0 +1,44 @@
+
+
+class Robot:
+    def __init__(self):
+        self.pots = []
+        self.max_pot_level = 2
+        self.current_pot = 0
+        self.pots.append({"level": 1})
+        self.pots.append({"level": 0})
+        self.pots.append({"level": 2})
+        self.pots.append({"level": 1})
+
+    def show_garden(self):
+        print "---"
+        for pot in self.pots:
+            print pot
+        print "---"
+        print ""
+
+    def move_right(self):
+        print "Move right"
+        if self.current_pot == len(self.pots) - 1:
+            return False
+        self.current_pot += 1
+        self.show_garden()
+        return True
+
+    def add_water(self):
+        print "Add water"
+        self.pots[self.current_pot]["level"] += 1
+        if self.pots[self.current_pot]["level"] > self.max_pot_level:
+            raise Exception("Oh no! You overfilled the pot!!")
+        self.show_garden()
+
+    def is_current_pot_full(self):
+        level = self.pots[self.current_pot]["level"]
+        return level == self.max_pot_level
+
+
+# while True:
+#     while not robot.is_current_pot_full():
+#         robot.add_water()
+#     if not robot.move_right():
+#         break
---
+++ Pigwarts/Year4_Potions/WaterPlants/robot_program.py
@@ -0,0 +1,8 @@
+from robot import Robot
+
+# robot.is_current_pot_full()
+# robot.add_water()
+# robot.move_right()
+
+robot = Robot()
+
