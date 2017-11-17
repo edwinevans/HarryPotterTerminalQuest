5688a28df27eb3c2bbecd196be4f9ed626b88467
Edwin Evans <ed@edwinevans.me>
2017-11-17 12:06:28 -0800

    misc

---
+++ Pigwarts/Review/turtle_stuff.py
@@ -0,0 +1,34 @@
+import turtle
+
+t = turtle.Turtle()
+s = turtle.Screen()
+p = turtle.Pen()
+
+def dunno():
+   s.bgcolor("black")
+   p.color("yellow")
+
+   p.forward(30)
+   p.right(90)
+   p.forward(30)
+   p.right(90)
+   p.forward(30)
+   p.right(90)
+   p.forward(30)
+   p.right(45)
+   p.forward(30)
+   p.right(45)
+   p.forward(30)
+   p.right(135)
+   p.forward(30)
+   p.left(45)
+   p.forward(30)
+   p.left(135)
+   p.forward(30)
+   p.left(45)
+   p.forward(30)
+
+
+dunno()
+
+turtle.done()
